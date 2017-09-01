6e2f7142e70e50c5d33859937da3a5c990a9b805
guillermaster <guillermaster@gmail.com>
2017-09-01 16:54:22 -0500

    added first react component

---
+++ NetCoreReact/Frontend/Homepage/Components/Demo/Demo.tsx
@@ -0,0 +1,17 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+import * as React from 'react';
+import './Demo.scss';
+
+class Demo extends React.Component<any, any> {
+    public render() {
+        return (
+            <div className="Demo">
+                <p>ASP.NET + React</p>
+            </div>
+        )
+    }
+}
+
+export default Demo;
---
+++ NetCoreReact/Frontend/Homepage/index.tsx
@@ -0,0 +1,9 @@
+﻿import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+
+import Demo from './Components/Demo/Demo';
+
+ReactDOM.render(
+    <Demo />,
+    document.getElementById('react-homepage-root')
+);
